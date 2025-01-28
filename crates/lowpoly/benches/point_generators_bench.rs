61e5bd1eb56289a20ace8b0f5b105e98a13dade0
Krishna Penukonda <krishna@hypotenuse.ai>
2025-01-28 12:10:39 +0800

    ⚡️ Added benchmark for point generators

---
+++ crates/lowpoly/benches/point_generators_bench.rs
@@ -0,0 +1,42 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use image::{DynamicImage, GenericImage};
+use lowpoly::point_generators::{generate_points_from_sobel, generate_random_points};
+use rand::Rng;
+
+fn bench_generate_points_from_sobel(c: &mut Criterion) {
+    let (height, width) = (400, 400);
+    let num_points = 1000;
+
+    // Generate an image containing random noise
+    let mut img = DynamicImage::new_rgb8(width, height);
+    let mut rng = rand::thread_rng();
+    for y in 0..height {
+        for x in 0..width {
+            let r: u8 = rng.gen();
+            let g: u8 = rng.gen();
+            let b: u8 = rng.gen();
+            img.put_pixel(x, y, image::Rgba([r, g, b, 255]));
+        }
+    }
+
+    c.bench_function("generate_points_from_sobel", |b| {
+        b.iter(|| {
+            let _res = generate_points_from_sobel(black_box(&img), black_box(num_points));
+        })
+    });
+}
+
+fn bench_generate_random_points(c: &mut Criterion) {
+    c.bench_function("generate_random_points", |b| {
+        b.iter(|| {
+            let _points = generate_random_points(black_box(1000), black_box(1000), black_box(500));
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_generate_points_from_sobel,
+    bench_generate_random_points
+);
+criterion_main!(benches);
